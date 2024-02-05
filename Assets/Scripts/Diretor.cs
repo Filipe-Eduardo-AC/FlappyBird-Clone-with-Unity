bbce037547f4873f39a2fcc6f365e572cc999e72
Filipe <filipeeacosta@gmail.com>
2024-02-05 19:31:11 -0300

    Final version, with Coop mode

---
+++ Assets/Scripts/Diretor.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diretor : MonoBehaviour
+{
+    private Aviao aviao;
+    private Pontuacao pontuacao;
+    private InterfaceGameOver interfaceGameOver;
+
+    protected virtual void Start()
+    {
+        this.aviao = Object.FindFirstObjectByType(typeof(Aviao)) as Aviao;
+        this.pontuacao = Object.FindFirstObjectByType(typeof(Pontuacao)) as Pontuacao;
+        this.interfaceGameOver = Object.FindFirstObjectByType(typeof(InterfaceGameOver)) as InterfaceGameOver;
+    }
+
+    public void FinalizarJogo()
+    {
+        Time.timeScale = 0;
+        this.pontuacao.SalvarRecorde();
+        this.interfaceGameOver.MostrarInterface();
+    }
+
+    public virtual void ReiniciarJogo()
+    {
+        this.interfaceGameOver.EsconderInterface();
+        Time.timeScale = 1;
+        this.aviao.Reiniciar();
+        this.DestruirObstaculos();
+        this.pontuacao.Reiniciar();
+    }
+
+    private void DestruirObstaculos()
+    {
+        Obstaculo[] obstaculos = (Obstaculo[])Object.FindObjectsByType(typeof(Obstaculo), FindObjectsSortMode.None);
+        foreach (Obstaculo obstaculo in obstaculos)
+        {
+            obstaculo.Destruir();
+        }
+    }
+}
---
+++ Assets/Scripts/VariavelCompartilhadaFloat.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "variavel compartilhada", menuName = "Variavel Compartilhada / Float")]
+public class VariavelCompartilhadaFloat : ScriptableObject
+{
+    public float valor;
+
+
+}
